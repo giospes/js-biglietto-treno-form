6c06c099879ea880aae93ddef62b2ceccbf6ddf4
giospes <giovannijacopo.spes@gmail.com>
2023-03-09 18:38:48 +0100

    done

--- js/script.js
+++ js/script.js
@@ -3,27 +3,43 @@ const discountS = .40
 const priceKm = .21
 const genButton = document.querySelector("#genera")
 const resButton = document.querySelector("#reset")
-let id, km, age;
+const biglietto = document.querySelector("#biglietto")
+let id, km, age, price, carrozza, codice;
 
 
-genButton.addEventListener("click", generate(priceKm, discountY, discountS))
+genButton.addEventListener("click", generate)
+
+resButton.addEventListener("click", reset)
 
 
-function generate(priceKm, discountYoung, discountSenior){
+function generate(){
+    biglietto.classList.remove("d-none")
     id = document.querySelector("#nome").value
     km = document.querySelector("#km").value
     age = document.querySelector("#eta").value
     km = parseInt(km)
+    
     price = km*priceKm
-    console.log(km)
-    console.log(age)
     if(age == 'Minorenne'){
-        price = price*(1 - discountYoung)
+        price = price*(1 - discountY)
     }
     if(age == 'Senior'){
-        price = price*(1 - discountSenior)
+        price = price*(1 - discountS)
     }
     price = Math.round(price * 100) / 100
-    console.log(price)
+    carrozza = Math.floor(Math.random()*20)
+    codice = Math.floor(Math.random()*10000)
+    document.getElementById("nome-display").innerHTML = id
+    document.getElementById("offerta-display").innerHTML = age
+    document.getElementById("carrozza-display").innerHTML = carrozza
+    document.getElementById("codice-display").innerHTML = codice
+    document.getElementById("price-display").innerHTML = price
 
+}
+
+function reset(){
+    biglietto.classList.add("d-none")
+    document.querySelector("#nome").value = ""
+    document.querySelector("#km").value = ""
+    document.querySelector("#eta").value = "Minorenne"
 }
